47a7e66a9f6f460ee00daf2d6f88aaa56f43994a
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-08-06 15:09:05 -0500

    misunderstood the directions; reworking file structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import CardList from './components/CardList'
 
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      user: []
+  };
+}
+
+componentDidMount() {
+  this.getUsers();
+};
+
+getUsers = () => {
+  axios
+  .get('https://api.github.com/users/BNMoyers')
+  .then(res => this.setState({user: res.data}))
+  .catch(err => {console.log('error', err);});
+};
+  }
+  
+  render(){  
   return (
     <div className="App">
       <header className="App-header">
        
       </header>
+      <CardList user={this.user}/>
     </div>
   );
+    
 }
 
 export default App;
