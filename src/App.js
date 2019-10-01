0ba053e3465d1fd877372f70be8de0a49b7ea13f
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-01 11:44:06 -0500

    initialized new CRA and cleaned up starter files

--- src/App.js
+++ src/App.js
@@ -1,48 +1,11 @@
 import React from 'react';
-import axios from 'axios';
-import UserCard from './components/Card';
-import 'semantic-ui-css/semantic.min.css'
+import './App.css';
 
-class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      userData: [],
-      userFollowers: []
-  };
-}
-
-componentDidMount() {
-  this.getUsers();
-  this.getFollowers();
-};
-
-getUsers = () => {
-  axios
-  .get('https://api.github.com/users/BNMoyers')
-  .then(res => this.setState({userData: res.data}))
-  .catch(err => {console.log('error', err);});
-};
-  
-  
-getFollowers = () => {
-  axios
-  .get('https://api.github.com/users/BNMoyers/followers')
-  .then(res => this.setState({userFollowers: res.data}))
-  .catch(err => {console.log('error', err);});
-};
-
-
-  render() {  
+function App() {
   return (
     <div className="App">
-      
-      <UserCard userData={this.state.userData}
-            userFollowers={this.state.userFollowers}/>
     </div>
   );
-    
-}
 }
 
 export default App;
